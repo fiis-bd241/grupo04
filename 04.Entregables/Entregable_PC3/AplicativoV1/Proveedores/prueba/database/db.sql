3b7b8a00f1aa7c330a34ad3c088e7c6495c451bd
QuispeCesar <114821149+QuispeCesar@users.noreply.github.com>
2024-06-05 10:57:53 -0500

    Delete 04.Entregables/Entregable_PC3/AplicativoV1/Proveedores/prueba/database directory

--- 04.Entregables/Entregable_PC3/AplicativoV1/Proveedores/prueba/database/db.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Pelicula(id INT,nombre VARCHAR(100),duracion VARCHAR(100),genero VARCHAR(100),PRIMARY KEY (id));
-
-INSERT INTO Pelicula(id, nombre, duracion, genero) VALUES (2, 'Marvel', '2.5','Accion');
-INSERT INTO Pelicula(id, nombre, duracion, genero) VALUES (3, 'Mars', '2.5','Sci');
-select * from Pelicula;
