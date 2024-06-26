6652aa97b0ca2feea0811bbb846f2362e0c1067f
AndyTaipe <142434091+TaipeAndy@users.noreply.github.com>
2024-06-26 04:23:14 -0500

    Add files via upload

---
+++ 04.Entregables/Entregable_PC4/MOD_MARKETING/archivo_marketing/aplicativo_marketing.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from gestor_mark.gui_app import Frame, barra_menu
+
+def main():
+    root = tk.Tk()
+    root.title("modulo de marketing")
+    root.resizable(0,0)
+    barra_menu(root)
+
+    app=Frame(root=root)
+
+    app.mainloop()
+
+
+if __name__ == '__main__':
+    main()
+    
