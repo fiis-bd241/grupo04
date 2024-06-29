c00cd08b87243e7cf0ca1c4b50a71cfe9d017780
QuispeCesar <114821149+QuispeCesar@users.noreply.github.com>
2024-06-29 18:37:03 -0500

    Delete 7. MONOGRAFIA/Capitulos/imagenes_cap16/MODdist/proyectodistribucion directory

--- 7. MONOGRAFIA/Capitulos/imagenes_cap16/MODdist/proyectodistribucion/controllers/cliente_controller.py
+++
@@ -1,8 +0,0 @@
-from models.reparto import Reparto
-
-class ClienteController:
-    def __init__(self):
-        self.reparto_model = Reparto()
-    
-    def listar_repartidores(self):
-        return self.reparto_model.get_repartidores()
